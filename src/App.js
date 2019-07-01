07721fdf98e07783c0f26ee85154cfc29c81fac2
Ben Donovan <gbsben@gmail.com>
2019-07-01 12:13:06 -0400

    for eric demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// font awesome library and initiation
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import Template from './components/MainTemplate';
+
 import './App.css';
+
+import Home from './pages/home';
+import Create from './pages/create';
+
+library.add(fas);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Template>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/create' component={Create} />
+          </Switch>
+        </Template>
+      </Router>
   );
 }
 
