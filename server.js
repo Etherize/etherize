534f2f4d6fe5a382a3a0bb8384628f379b1e25aa
Ian Philips <iansp@MacBook-Pro.local>
2020-01-13 15:40:47 -0700

    certbot

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-const certbotEndPoint = process.env.certbotendpoint;
-const certbotKey = process.env.certbotkey;
+const certbotEndPoint = process.env.certbotendpoint || "/cerbot-unused";
+const certbotKey = process.env.certbotkey || "";
 
 app.prepare()
     .then(() => {
