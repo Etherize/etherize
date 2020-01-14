ed358fc7e46c2fbf870d939d14c931698f0eb7b8
Ian Philips <iansp@MacBook-Pro.local>
2020-01-13 17:48:28 -0700

    Splash page changes - certbot fix

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const next = require('next')
+const express = require('express');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
-const certbotEndPoint = process.env.certbotendpoint || "/cerbot-unused";
-const certbotKey = process.env.certbotkey || "";
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+// const certbotEndPoint = process.env.certbotendpoint || "/cerbot-unused";
+// const certbotKey = process.env.certbotkey || "";
 
 app.prepare()
     .then(() => {
         const server = express();
 
-        server.get(certbotEndPoint, (req, res) => {
-            res.send(certbotKey);
-        });
+        // server.get(certbotEndPoint, (req, res) => {
+        //     res.send(certbotKey);
+        // });
 
         server.get('*', (req, res) => {
             return handle(req, res)
         })
 
         server.listen(3000, (err) => {
-            if (err) throw err
+            if (err) throw err;
             console.log('> Ready on http://localhost:3000')
         })
     })
     .catch((ex) => {
-        console.error(ex.stack)
+        console.error(ex.stack);
         process.exit(1)
     })
--- src/components/EntityServices.js
+++ src/components/EntityServices.js
@@ -15,7 +15,6 @@ import {
     MDBAnimation,
     MDBCardGroup
 } from "mdbreact";
-import Typed from 'typed.js';
 import TypedText from "./TypedText";
 import "./EntityServices.css";
 import Link from 'next/link';
@@ -31,22 +30,7 @@ import BannerHeader from "./BannerHeader";
 import portal from "../assets/img/portal-6.svg"
 
 
-
-// const EntityServices = () => (
 class EntityServices extends React.Component {
-
-    constructor() {
-        super();
-        this.state = {
-            isFlipped: false
-        };
-        this.handleHoverFlip = this.handleHoverFlip.bind(this);
-    }
-
-    handleHoverFlip(e) {
-        e.preventDefault();
-        this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
-    }
 
     render() {
 
@@ -56,31 +40,22 @@ class EntityServices extends React.Component {
                 <BannerHeader/>
                 <MDBJumbotron fluid >
                     <MDBContainer className={"mt-5"}>
-                        <MDBRow>
                             <MDBRow>
                                 <MDBCol md={"4"} middle={true}>
-                                    <MDBCard border={"0"} className={"my-auto"}>
-
                                         <MDBAnimation type={"fadeInRight"} duration={"2s"}>
                                             <MDBCardTitle className="jumbo-title text-center">
-                                                hybrid formation portal
+                                                Hybrid Formation Portal
                                             </MDBCardTitle>
                                           </MDBAnimation>
                                 </MDBCol>
                                 <MDBCol md={"4"} sm={"4"} xs={"2"} middle={true}>
                                     <MDBAnimation type={"fadeIn"} duration={"2s"}>
-
-
                                                <img src={portal} className="img-fluid" alt="" />
-
                                     </MDBAnimation>
 
                                 </MDBCol>
                                 <MDBCol md={"4"} sm={"6"} xs={"2"} middle={true}>
-
-
                                         <MDBAnimation type={"fadeInLeft"} delay="2" duration={"2s"}>
-
                                             <MDBCardTitle className="text-center">
                                             make your first <br/>
                                             <TypedText
@@ -93,58 +68,74 @@ class EntityServices extends React.Component {
                                             </MDBCardTitle>
                                         </MDBAnimation>
 
+
                                 </MDBCol>
                                 <MDBContainer className={"text-center"}>
-                                  <MDBAnimation delay="66s" type={"fadeOutLeft"}>
-                                    <MDBAnimation delay="28s" type={"fadeInDown"} duration={"2s"}>
 
-                                        <MDBCardTitle className="title pt-3 m-5 entity-type-description bold">
-                                            The Portal Beckons
+                                <MDBCol className={"text-center mb-2 mt-5"} lg={"12"}>
+                                    <MDBAnimation type={"fadeIn"}>
+                                        <MDBCardTitle>
+                                            Rally your team,
+                                            harness decentralization,
+                                            govern towards abundance. <br/> <br/>
+                                            Go ahead,
                                         </MDBCardTitle>
 
                                     </MDBAnimation>
-                                      <MDBAnimation delay="31s" type={"fadeIn "} duration={"2s"}>
-                                    <h2> in your mind the words form:</h2>
-                                    <br/>
+
+                                    <MDBAnimation type="fadeIn" >
+                                        <MDBBtn size="lg" href={"#products"} className={"btn-primary mt-4"}>
+                                            Open the Portal
+                                        </MDBBtn>
+
                                     </MDBAnimation>
 
-                                    <MDBAnimation type="fadeInLeft" delay="33s" >
-                                    <TypedText
-                                        strings={[
-                                        '^35000are you ready?^4000',
-                                        'to rally your team?^3000',
-                                        'to harness decentralization?^3000',
-                                        'to govern towards abundance?^2000',
-                                        'to govern towards abundance^1000.^10000',
-                                        ''
-
-
-                                      ]}
-                                      typeSpeed={['1s']}
-                                    />
-                                    </MDBAnimation>
-                                    <br/>
-                                    </MDBAnimation>
+                                </MDBCol>
                                 </MDBContainer>
+
+                                {/*<MDBContainer className={"text-center"}>*/}
+                                {/*  <MDBAnimation delay="66s" type={"fadeOutLeft"}>*/}
+                                {/*    <MDBAnimation delay="28s" type={"fadeInDown"} duration={"2s"}>*/}
+
+                                {/*        <MDBCardTitle className="title pt-3 m-5 entity-type-description bold">*/}
+                                {/*            The Portal Beckons*/}
+                                {/*        </MDBCardTitle>*/}
+
+                                {/*    </MDBAnimation>*/}
+                                {/*      <MDBAnimation delay="31s" type={"fadeIn "} duration={"2s"}>*/}
+                                {/*    <h2> in your mind the words form:</h2>*/}
+                                {/*    <br/>*/}
+                                {/*    </MDBAnimation>*/}
+
+                                {/*    <MDBAnimation type="fadeInLeft" delay="33s" >*/}
+                                {/*    <TypedText*/}
+                                {/*        strings={[*/}
+                                {/*        '^35000are you ready?^4000',*/}
+                                {/*        'to rally your team?^3000',*/}
+                                {/*        'to harness decentralization?^3000',*/}
+                                {/*        'to govern towards abundance?^2000',*/}
+                                {/*        'to govern towards abundance^1000.^10000',*/}
+                                {/*        ''*/}
+
+
+                                {/*      ]}*/}
+                                {/*      typeSpeed={['1s']}*/}
+                                {/*    />*/}
+                                {/*    </MDBAnimation>*/}
+                                {/*    <br/>*/}
+                                {/*    </MDBAnimation>*/}
+                                {/*</MDBContainer>*/}
 
                                 {/* Fun Features row*/}
 
 
-                            </MDBRow>
                         </MDBRow>
                     </MDBContainer>
                 </MDBJumbotron>
 
                     <MDBRow className={"mt-1 mainBackground2  fluid"}>
                         {/*/!*Title*!/*/}
-                        <MDBCol className={"text-center mb-2"} lg={"12"}>
-                            <MDBAnimation type="fadeIn" delay="62s">
-                                <Link href={"/"}>
-                                    <strong className="mt-4 logoText">ETHERIZE</strong>
-                                </Link>
-                                <div className="mt-4"/>
-                            </MDBAnimation>
-                        </MDBCol>
+
 
                         {/*CARDS*/}
                         {/*Card layout/design loosely based on : https://0x.org/launch-kit*/}
@@ -153,24 +144,21 @@ class EntityServices extends React.Component {
 
                                 {/*Card */}
                                 <MDBCol lg="12" className="mb-5 mt-5">
-                                    <MDBCardGroup>
+                                    <MDBCardGroup id={"products"}>
                                         {/*LEFT SIDE*/}
                                         <MDBCol lg={"6"}>
                                             <MDBCard cascade className={"text-left h-100"} >
                                                 <MDBCardBody cascade className={"mt-5 mb-5 ml-3"}>
                                                     <MDBCardTitle className={"mb-3"}>
-                                                        <div>
-                                                        <p className="card-title text-center h1">Form Hybrid Entity</p>
-
-                                                        </div >
+                                                        <p className="card-title h1">Form Hybrid Entity</p>
+                                                            {/*from $700 or 5<MDBIcon fab icon="ethereum"/>*/}
                                                     </MDBCardTitle>
 
                                                     <MDBCardText className={"mb-5 mt-5"}>
                                                         Bring your own DAO or wallet address, or allow us to help you create one.
                                                         We will pair it to a legal entity, allowing the same organization to conduct business with both mortals and etheric entities.
                                                         Our service automates the paperwork involved in forming legal entities and blockchain entities.
-
-
+                                                        <br/>
                                                         <MDBBadge
                                                             className="mr-2 mt-2 badge-info"> Wyoming</MDBBadge><MDBBadge
                                                         className="mr-2 mt-2 badge-info">Non-Profit or
@@ -178,22 +166,14 @@ class EntityServices extends React.Component {
                                                         Network</MDBBadge>
 
                                                     </MDBCardText>
-                                                    <MDBRow>
-                                                    <MDBCol>
-                                                        from $700 or 5<MDBIcon fab icon="ethereum"/>
                                                     <MDBBtn size="lg" href={"/create"} className={"btn-secondary"}>
                                                         Begin <MDBIcon icon="bolt"/>
-                                                        </MDBBtn>
-                                                        </MDBCol>
-                                                        <MDBCol>
-                                                          Read more.
-                                                        </MDBCol>
-                                                        </MDBRow>
+                                                    </MDBBtn>
                                                 </MDBCardBody>
                                             </MDBCard>
                                         </MDBCol>
                                         <MDBCol lg={"6"}>
-                                            <MDBCardImage className="img-fluid" style={{maxWidth:"125%"}}  src={self} waves/>
+                                            <MDBCardImage className="img-fluid" style={{maxWidth:"125%"}}  src={self}/>
                                         </MDBCol>
                                     </MDBCardGroup>
                                 </MDBCol>
@@ -207,10 +187,8 @@ class EntityServices extends React.Component {
                                             <MDBCard cascade className={"text-left h-100"} >
                                                 <MDBCardBody cascade className={"mt-5 mb-5 ml-3"}>
                                                     <MDBCardTitle className={"mb-3"}>
-                                                        <div/>
-                                                        <p className="card-title h3">Form Legal Entity</p>
-                                                        from $500 | 4 <MDBIcon fab icon="ethereum"/>
-                                                        <div/>
+                                                        <p className="card-title h1">Form Legal Entity</p>
+                                                        {/*from $500 | 4 <MDBIcon fab icon="ethereum"/>*/}
                                                     </MDBCardTitle>
 
                                                     <MDBCardText className={"mb-5 mt-5"}>
@@ -220,8 +198,7 @@ class EntityServices extends React.Component {
                                                             and Formation Certificate will be sent to your inbox. Enjoy
                                                             the lowest taxes and strongest protections in the US. One
                                                             consultation is included to help you get things rolling.
-
-
+                                                        <br/>
                                                         <MDBBadge
                                                             className="mr-2 mt-2 badge-info"> Wyoming</MDBBadge><MDBBadge
                                                         className="mr-2 mt-2 badge-info">Non-Profit or LLC</MDBBadge>
@@ -233,7 +210,7 @@ class EntityServices extends React.Component {
                                             </MDBCard>
                                         </MDBCol>
                                         <MDBCol lg={"6"}>
-                                            <MDBCardImage className="img-fluid fullWidth" src={sleeping} waves/>
+                                            <MDBCardImage className="img-fluid fullWidth" src={sleeping}/>
                                         </MDBCol>
                                     </MDBCardGroup>
                                 </MDBCol>
@@ -245,33 +222,30 @@ class EntityServices extends React.Component {
                                     {/*LEFT SIDE*/}
                                         <MDBCol lg={"6"}>
                                          <MDBCard cascade className={"text-left h-100"} >
-                                            <MDBCardBody cascade className={"mt-5 mb-5 ml-3"}>
-                                            <MDBCardTitle className={"mb-3"}>
-                                                <div/>
-                                                <p className="card-title h3">Design Entity</p>
-                                                <div/>
-                                            </MDBCardTitle>
-
+                                             <MDBCardBody cascade className={"mt-5 mb-5 ml-3"}>
+                                             <MDBCardTitle className={"mb-3"}>
+                                                 <p className="card-title h1">Design Entity</p>
+                                             </MDBCardTitle>
                                             <MDBCardText className={"mb-5 mt-5"}>
                                                  Our wizard🧙 generates documents for you & co-founders to review and, if
                                                     the stars align, sign. Documents include Articles of Incorporation
                                                     and an Operating Agreement. Afterwards you can submit the filings yourself
                                                     or hire Etherize to do it for you later.
-
+                                                <br/>
                                                 <MDBBadge className="mr-2 mt-2 badge-info"> Any
                                                     State</MDBBadge><MDBBadge className="mr-2 mt-2 badge-info">Any Crypto
-                                                    Network</MDBBadge><MDBBadge className="mr-2 mt-2 badge-info">Non-Profitor
+                                                    Network</MDBBadge><MDBBadge className="mr-2 mt-2 badge-info">Non-Profit or
                                                     LLC</MDBBadge>
-                                            </MDBCardText>
 
-                                                <MDBBtn size="lg" href={"/create"} className={"btn-secondary"}>
-                                                    Begin <MDBIcon icon="bolt"/>
-                                                </MDBBtn>
+                                            </MDBCardText>
+                                                 <MDBBtn size="lg" href={"/create"} className={"btn-secondary"}>
+                                                     Begin <MDBIcon icon="bolt"/>
+                                                 </MDBBtn>
                                             </MDBCardBody>
                                             </MDBCard>
                                         </MDBCol>
                                         <MDBCol lg={"6"}>
-                                            <MDBCardImage className="img-fluid fullWidth" src={awaken} waves/>
+                                            <MDBCardImage className="img-fluid fullWidth" src={awaken} />
                                         </MDBCol>
                                     </MDBCardGroup>
                                 </MDBCol>
